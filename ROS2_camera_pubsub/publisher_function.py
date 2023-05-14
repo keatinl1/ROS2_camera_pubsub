e3f3787a9691a3faa59c6fbfc03066a127994687
Luke Keating <keatinl1@tcd.ie>
2023-05-14 21:05:04 +0100

    working

--- ROS2_camera_pubsub/publisher_function.py
+++ ROS2_camera_pubsub/publisher_function.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 import cv2
+import time as time
 
 class CameraPublisherNode(Node):
     def __init__(self):
@@ -18,22 +19,36 @@ class CameraPublisherNode(Node):
 
     def publish_camera_feed(self):
         # Capture a frame from the camera
-        ret, frame = self.cap.read()
-        if not ret:
-            self.get_logger().error("Failed to capture camera frame")
-            return
+        t_at_start = time.time()
+
+        _, frame = self.cap.read()
+
+        compressed_frame = self.compress_jpeg(frame)
 
         # Convert the frame to a ROS2 Image message
         img_msg = Image()
         img_msg.header.stamp = self.get_clock().now().to_msg()
         img_msg.encoding = "bgr8"
-        img_msg.width = frame.shape[1]
-        img_msg.height = frame.shape[0]
-        img_msg.step = frame.shape[1] * 3
-        img_msg.data = frame.tobytes()
+        img_msg.width = compressed_frame.shape[1]
+        img_msg.height = compressed_frame.shape[0]
+        img_msg.step = compressed_frame.shape[1] * 3
+        img_msg.data = compressed_frame.tobytes()
+
+        print(f'FPS: {1 / (time.time() - t_at_start)}')
 
         # Publish the image message
         self.publisher_.publish(img_msg)
+
+    def compress_jpeg(self, frame, quality=80):
+
+        # Define the JPEG compression quality
+        jpeg_quality = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
+
+        # Compress the image using JPEG
+        _, compressed_img = cv2.imencode(".jpg", frame, jpeg_quality)
+
+        return cv2.imdecode(compressed_img, cv2.IMREAD_COLOR)
+
 
 def main(args=None):
     rclpy.init(args=args)
